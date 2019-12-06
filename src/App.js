76711b635bfab5cfd7c528028880f79648ae55d9
Reem H <reemhamoui92@gmail.com>
2019-12-06 18:11:08 -0500

    almost there with section two functionality

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import SectionOne from './SectionOne';
 import SectionTwo from './SectionTwo';
 import SectionThree from './SectionThree';
 
-class App extends Component{
+class App extends Component {
   constructor() {
     super()
     this.state = {
       userInput: "",
       showSectionOne: true,
-      showSectionTwo : true,
-      showSectionThree : true,
-      paintingColor: '',
+      showSectionTwo: true,
+      showSectionThree: true,
+      paintingColor: "",
       chosenBrand: "orly",
       // hold the 28 colors
       finalPainting: {},
-      chosenColor : "",
+      chosenColor: "",
       top3Products: ['product1', 'product2', 'product3'].join(", "),
       colorsArray: [],
-      colorsArray2  : {
+      colorsArray2: {
         1: "#737C84",
         2: "#FBF6E1",
         darkTeal: "#2F4F4F",
@@ -90,133 +90,100 @@ class App extends Component{
           "zorah",
           "zorah biocosmetiques",
         ],
-        brandObject: {
+        brandObject: {},
+        brandArray: [],
 
-        }
-      
+
       }
-      
+
 
     }
   }
 
-
-
   componentDidMount() {
-    
-        // axios({
-        //   method: 'GET',
-        //   url: `https://www.rijksmuseum.nl/api/en/collection?key=kwQgDPpO&f.normalized32Colors.hex=${matchedColor.value}`,
-        //   dataResponse: 'json',
-        //   params: {
-        //     // key: 'e2KwL8qU',
-        //     // normalized32Colors: '%23FF0000'
 
-        //   }
-        // })
-        //   .then((data) => {
-        //     // this.setState({ arts: data.data.artObjects[0]})
-        //     // console.log(this.state.arts);
-        //     console.log('museum', data)
-        //   })
+    // axios({
+    //   method: 'GET',
+    //   url: `https://www.rijksmuseum.nl/api/en/collection?key=kwQgDPpO&f.normalized32Colors.hex=${matchedColor.value}`,
+    //   dataResponse: 'json',
+    //   params: {
+    //     // key: 'e2KwL8qU',
+    //     // normalized32Colors: '%23FF0000'
+
+    //   }
+    // })
+    //   .then((data) => {
+    //     // this.setState({ arts: data.data.artObjects[0]})
+    //     // console.log(this.state.arts);
+    //     console.log('museum', data)
+    //   })
   }
 
 
   makeUpCall = () => {
     axios({
-        method: 'GET',
-        url: `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${this.state.chosenBrand}`,
-        dataResponse: 'json'
-      })
+      method: 'GET',
+      url: `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${this.state.chosenBrand}`,
+      dataResponse: 'json'
+    })
       .then((makeUpData) => {
-        
+
         const arrayOfProducts = []
-        const loopedColors = []
-        const brandInfo = {
-          product1: {
-            
-          },
-          product2: {},
-          product3: {},
-            // // productDetails : {
-            // productName: '',
-            // color1: {
-            //   colorName: '',
-            //   hexValue: '',
-            // },
-            // color2: {
-            //   colorName: '',
-            //   hexValue: '',
-            // }
-          
-        }
+        const brandInfo = []
         
-        makeUpData.data.map((products) => {
+        makeUpData.data.map((products, index) => {
+          const indexNumProduct = index;
+          if (products.product_colors.length >= 7) {
+            const colorObject = products.product_colors.map((color, index) => {
+              if (index < 8) {
 
-        // const productColor = '',
-
-        if (products.product_colors.length >= 7) {
-            // arrayOfProducts.push(products.name);
-            // loopedColors.push(products.product_colors)
-            const colorObject = {
-              hex: '',
-              nameColor: '',
-            }
-            products.product_colors.map((color) => {
-              colorObject.hex = color.hex_value;
-              colorObject.nameColor = color.colour_name;
+                return {
+                  hex: color.hex_value,
+                  nameColor: color.colour_name,
+                }
+              }
             })
-          brandInfo.product1.productName = products.name;
-          brandInfo.product1.color1 = colorObject;
-
-          }       
-          // this.setState({
-          //   brandObject = brandInfo
-          //   // }
-          // })
-
-        // loopedColors.push(products.prod)
-        // products.product_colors.map((colorMap) => {
-        //     loopedColors.push(colorMap.hex_value)
-        //   })
-
-
+            brandInfo[indexNumProduct] = colorObject;
+            brandInfo[indexNumProduct].productName = products.name;
+            // brandArray.push(brandInfo[indexNumProduct])
+            // brandArray.push(brandInfo[indexNumProduct])
+          }
+          
+          
         })
         console.log(brandInfo)
-
-        // loopedColors.map((color) => {
-        //   if ()
-        // })
+        // brandArray.push(brandInfo)
         
-        this.setState({
-          top3Products: arrayOfProducts.join(", ")
-        })
-
+        
+        // this.setState({
+        //   top3Products: arrayOfProducts.join(", "),
+        //   brandArray: brandInfo,
+        // })
 
       })
   }
 
-      // nearestColorFunction = () => {        
-      //   const nearestColor = require('nearest-color').from(this.state.colorsArray);
-      //   // (console.log(nearestColor('#f01')))
-      //   // console.log('this is the nearest color', nearestColor)
-      //   let matchedColor = nearestColor(colorFinal);
-      //   console.log(matchedColor.value)
-      // }
+  // nearestColorFunction = () => {        
+  //   const nearestColor = require('nearest-color').from(this.state.colorsArray);
+  //   // (console.log(nearestColor('#f01')))
+  //   // console.log('this is the nearest color', nearestColor)
+  //   let matchedColor = nearestColor(colorFinal);
+  //   console.log(matchedColor.value)
+  // }
 
 
   render() {
     return (
       <div className="App">
         <SectionOne />
-        { 
+        {
           this.state.showSectionTwo === true
-          ?( 
+            ? (
 
-              <SectionTwo makeUpCallProp={this.makeUpCall} chosenBrandProp={this.state.chosenBrand} top3ProductsProp={this.state.top3Products} colorsArrayProp={this.state.colorsArray}/>
-              
+              <SectionTwo makeUpCallProp={this.makeUpCall} chosenBrandProp={this.state.chosenBrand} top3ProductsProp={this.state.top3Products} colorsArrayProp={this.state.colorsArray} brandProductsAndColorsProp={this.state.brandArray} />
+
             )
-          :null
+            : null
         }
         {
           (
@@ -224,14 +191,14 @@ class App extends Component{
               ?
 
               (<SectionThree />)
-            :
+              :
               (null)
           )
-          
+
         }
       </div>
     );
-    
+
   }
 }
 
--- src/SectionTwo.js
+++ src/SectionTwo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-​
+
 class SectionTwo extends Component {
     constructor() {
         super();
@@ -8,18 +8,37 @@ class SectionTwo extends Component {
             color: '#9C7667',
         }
     }
-​
-    
+        appendColors = () => {
+            for (let i in this.props.brandProductsAndColorsProp) {
+                console.log('hiiii', i)
+                return i
+            }
+            this.appendColors();
+        }
     render() {
         return (
             <div>
-                {console.log(this.props.chosenBrandProp)}
+                )
+                {/* {console.log(this.props.chosenBrandProp)} */}
+                {
+                
+                    
+                }
                 <h1>You chose the brand: {this.props.chosenBrandProp}</h1>
                 <p>Choosing {this.props.chosenBrandProp} is a great idea! Personally, we love {this.props.top3ProductsProp}</p>
                 {
-                    /* 
-                    <p>Color 1 {this.props.colorsArrayProp[0]}. Color 2 {this.props.colorsArrayProp[1]}. Color 3 {this.props.colorsArrayProp[2]}. Color 4 {this.props.colorsArrayProp[3]}. Color 5 {this.props.colorsArrayProp[4]}. Color 6 {this.props.colorsArrayProp[6]}. Color 7 {this.props.colorsArrayProp[7]}</p> 
-                    */
+                    
+                    
+                    // <p>Color 1 {this.props.brandProductsAndColorsProp}. Color 2 {this.props.brandProductsAndColorsProp}. Color 3 {this.props.brandProductsAndColorsProp}. Color 4 {this.props.brandProductsAndColorsProp}. Color 5 {this.props.brandProductsAndColorsProp}. Color 6 {this.props.brandProductsAndColorsProp}. Color 7 {this.props.brandProductsAndColorsProp}</p> 
+                    // colorsssssss 
+                    // this.props.brandProductsAndColorsProp.map((eeee) => {
+
+                    //     // return (
+                    //     //     <p> {eeee.product_name} </p>  
+                    //     // )
+                    //     console.log(eeee)
+                    // })
+                    
                 }
                 
                 <button onClick={this.props.makeUpCallProp}>howdy</button>
@@ -27,7 +46,7 @@ class SectionTwo extends Component {
             </div>
         )
     }
-​
+
 }
-​
+
 export default SectionTwo;
