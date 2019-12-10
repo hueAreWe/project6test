2d1eae5e13b67c8679c65584a8da4a454ab53375
Tommy <tomstuart123@hotmail.co.uk>
2019-12-10 17:56:59 -0500

    carousel fixed

--- src/SectionTwo.js
+++ src/SectionTwo.js
@@ -6,36 +6,35 @@ class SectionTwo extends Component {
         super();
         this.state = {
             color: '',
-            counter: 0
+            counter: 0,
+            // initialLoad: this.props.sectionTwoPageLoad,
         }
     } 
 
 
     counterClickAdd = () => {
-        this.setState({
-            counter: this.state.counter + 1
-        })
-        if (this.state.counter === this.props.brandArray.length) {
-            console.log('the lengths are now the same!!!')
+        if (this.state.counter < (this.props.brandArray.length - 1)) {
             this.setState({
-                counter: 0
+                counter: this.state.counter + 1
+            })
+        } else {
+            this.setState({
+                counter: 0,
             })
         }
-        console.log('this is the counter now after i click the arrow',this.state.counter)
     }
 
-    // counterClickSub = () => {
-    //     this.setState({
-    //         counter: this.state.counter - 1
-    //     })
-    //     if (this.state.counter === this.props.brandArray.length) {
-            
-    //         this.setState({
-    //             counter: 0
-    //         })
-    //     }
-    //     console.log('this is the counter now after i click the arrow', this.state.counter)
-    // }
+    counterClickSub = () => {
+        if (this.state.counter > 0) {
+            this.setState({
+                counter: this.state.counter - 1
+            })
+        } else {
+            this.setState({
+                counter: this.props.brandArray.length - 1,
+            })
+        }
+    }
 
     
     render() {
@@ -47,13 +46,13 @@ class SectionTwo extends Component {
 
                 {
                     this.props.brandArray.map((product, index) => {
-                        // console.log('this is the brand array thingy', this.props.brandArray);
-                        console.log('indexy', index)
-                        //make a function that calls upon index in the array. have the onclick function in our arrows below call upon the counterClick above, enabling counter to increase by 1 (if right arrow) or decrease by 1 (if left arrow). once done, this will increase our counter and we will compare our index to the number on the counter. if the index number is = to counter, then we will only show what is equal to counter and visually hide everything else (take that from the setup snippet)
+                        // make a function that calls upon index in the array. have the onclick function in our arrows below call upon the counterClick above, enabling counter to increase by 1 (if right arrow) or decrease by 1 (if left arrow). once done, this will increase our counter and we will compare our index to the number on the counter. if the index number is = to counter, then we will only show what is equal to counter and visually hide everything else (take that from the setup snippet)
                         
                         
-                        const productName = product.slice(-1)[0]
-                        if (this.state.counter === index) {
+                        const productName = product.slice(-2)[0]
+                        const productImgLink = product.slice(-1)[0]
+                    
+                        if (this.state.counter === (index)) {
                                 return (
                                 
                             
@@ -75,7 +74,7 @@ class SectionTwo extends Component {
                                                 
                                             </div>
                                             <div className="makeUpImg">
-                                                <img src={this.props.productImageProp} alt={`${this.props.chosenBrandProp}'s ${productName}`} />
+                                                <img src={productImgLink} alt={`${this.props.chosenBrandProp}'s ${productName}`} />
                                             </div>
                                     
                                         </div>
