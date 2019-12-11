ae3f801b96642273cb128e9f901aff0ffe9918e0
s7illen <s7illen@gmail.com>
2019-12-10 21:16:45 -0500

    Updates

---
+++ precache-manifest.dc42408af0d5beb479a025eb03dd3a75.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7070d250e433ca20d69cb45b77ddc279",
+    "url": "/hueAreYou/index.html"
+  },
+  {
+    "revision": "7b3c8c2b4b80bfbd691f",
+    "url": "/hueAreYou/static/css/main.8af9b91e.chunk.css"
+  },
+  {
+    "revision": "87e6c683f8d22f028f7b",
+    "url": "/hueAreYou/static/js/2.6c3dfcaf.chunk.js"
+  },
+  {
+    "revision": "7b3c8c2b4b80bfbd691f",
+    "url": "/hueAreYou/static/js/main.5bf597df.chunk.js"
+  },
+  {
+    "revision": "7d9905456f37dc154f9f",
+    "url": "/hueAreYou/static/js/runtime-main.4b584668.js"
+  },
+  {
+    "revision": "d94f960c51814cbb10fc335f5c80d3a6",
+    "url": "/hueAreYou/static/media/backgroundImg.d94f960c.png"
+  },
+  {
+    "revision": "468cf4b7a35c24671795f6e909210683",
+    "url": "/hueAreYou/static/media/brushes.468cf4b7.jpg"
+  },
+  {
+    "revision": "d1152b3fbfd4863d450b3f75557d67e6",
+    "url": "/hueAreYou/static/media/easelPainting.d1152b3f.gif"
+  },
+  {
+    "revision": "250f9e0d2de4c746f7eee3674dc4f41e",
+    "url": "/hueAreYou/static/media/galleryBack.250f9e0d.png"
+  },
+  {
+    "revision": "249858b77d6396da12055bc8aceca1c7",
+    "url": "/hueAreYou/static/media/makeupPaintPalette.249858b7.png"
+  },
+  {
+    "revision": "c57b7deae2ec9758a6aa71130a3645f7",
+    "url": "/hueAreYou/static/media/productBottom.c57b7dea.png"
+  },
+  {
+    "revision": "6e20e8aabeb60b7af58321d65bf3cf57",
+    "url": "/hueAreYou/static/media/productTop.6e20e8aa.png"
+  },
+  {
+    "revision": "341ecc6a6f2f8e1f6c804624601aae82",
+    "url": "/hueAreYou/static/media/sectionOneBack.341ecc6a.jpg"
+  },
+  {
+    "revision": "36820610c6f2499e02992130913840bc",
+    "url": "/hueAreYou/static/media/sectionTwoPreload.36820610.gif"
+  },
+  {
+    "revision": "802ee20fb36d6b6293481c8487724ccd",
+    "url": "/hueAreYou/static/media/title-02.802ee20f.png"
+  }
+]);
